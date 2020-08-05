ed1f0b5851663109dde133a23b73b351c7460a9a
mike_visser <mpviss@gmail.com>
2020-08-05 11:24:36 -0400

    item form onChange function added

--- src/components/Catalogue/ItemForm.js
+++ src/components/Catalogue/ItemForm.js
@@ -18,8 +18,13 @@ export class ItemForm extends Component {
     }
 
 
-    onChange = () =>{
+    onChange = (e) =>{
+        e.preventDefault()
 
+        this.setState({
+            [e.target.name]: e.target.value
+
+        })
 
     }
 
@@ -28,25 +33,29 @@ export class ItemForm extends Component {
 
     }
 
+    cancelForm = () =>{
+        
+    }
+
     render() {
         return (
             <form className="item-form">
                 <div className="item-form-image">
                     <h3 style={{color: "white"}}>Placeholder</h3>
-                    <input className="item-form-add-image" type="text" placeholder="Image Url" />
+                    <input className="item-form-add-image" type="text" name="image" placeholder="Image Url" onChange={this.onChange} />
                 </div>
                 <div className="item-form-row-one">
                     <h2>Enter the details for the new item.</h2>
-                    <input className="item-form-name item-form-input" type="text" placeholder="Item Name" />
-                    <textarea className="item-form-detail item-form-input" placeholder="Item Description" />
+                    <input className="item-form-name item-form-input" type="text" name="name" placeholder="Item Name" onChange={this.onChange} />
+                    <textarea className="item-form-detail item-form-input" name="description" placeholder="Item Description" onChange={this.onChange} />
                 </div>
                 <div className="item-form-row-two">
                     <div className="item-form-row-two-unit-price">
-                        <input className="item-form-unit" type="text" placeholder="Unit Size" />
-                        <input className="item-form-unit" type="text" placeholder="Unit Price" />
+                        <input className="item-form-unit" type="number" name="unit" placeholder="Unit Size" onChange={this.onChange} />
+                        <input className="item-form-unit" type="number" name="price" placeholder="Unit Price" onChange={this.onChange} />
                     </div>
                     <div className="item-form-row-two-inventory">
-                    <input className="item-form-inventory" type="text" placeholder="Inventory Count" />
+                    <input className="item-form-inventory" type="number" name="inventory" placeholder="Inventory Ct." onChange={this.onChange} />
                     </div>
                 </div>
                 <div className="item-form-buttons">
