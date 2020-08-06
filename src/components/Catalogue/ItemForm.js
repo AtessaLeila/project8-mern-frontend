843c1f30ffa6bba37d6fa4e093d7cc3b560eb836
mike_visser <mpviss@gmail.com>
2020-08-06 10:36:44 -0400

    updating form to accept item details

--- src/components/Catalogue/ItemForm.js
+++ src/components/Catalogue/ItemForm.js
@@ -14,6 +14,8 @@ export class ItemForm extends Component {
             ,inventory: 0
             ,image: ""
             ,success: false
+            ,ready: false
+            ,placeholder: {}
         }
     }
 
@@ -23,15 +25,14 @@ export class ItemForm extends Component {
             .then(res => res.json())
             .then(res =>{
                 this.setState({
-                    name: ""
-                    ,description: ""
-                    ,category: ""
-                    ,unit: 0
-                    ,price: 0
-                    ,inventory: 0
-                    ,image: ""
-                    ,placeholder: {}
+                   placeholder: res
+                   ,ready: true
                 })
+            })
+        }
+        else {
+            this.setState({
+                ready: true
             })
         }
     }
@@ -80,16 +81,16 @@ export class ItemForm extends Component {
              <div><h2>Success!</h2></div>
          )       
         }
-        else{
+        else if (this.state.ready === true && this.props.id === true){
         return (
             <form className="item-form" onSubmit={this.onSubmit}>
                 <div className="item-form-image">
                     <h3 style={{color: "white"}}>Placeholder</h3>
-                    <input className="item-form-add-image" type="text" name="image" placeholder="Image Url" onChange={this.onChange} />
+                    <input className="item-form-add-image" type="text" name="image" placeholder={this.state.placeholder.image} onChange={this.onChange} />
                 </div>
                 <div className="item-form-row-one">
                     <h2>Enter the details for the new item.</h2>
-                    <input className="item-form-name item-form-input" type="text" name="name" placeholder="Item Name" onChange={this.onChange} />
+                    <input className="item-form-name item-form-input" type="text" name="name" onChange={this.onChange} />
                     <input className="item-form-category item-form-input" type="text" name="category" placeholder="Item Category" onChange={this.onChange} />
                     <textarea className="item-form-detail item-form-input" name="description" placeholder="Item Description" onChange={this.onChange} />
                 </div>
@@ -108,7 +109,33 @@ export class ItemForm extends Component {
                 </div>
             </form>
         )
-        }
+        } else if(this.state.ready === true && this.props.id === false) {return(
+            <form className="item-form" onSubmit={this.onSubmit}>
+                <div className="item-form-image">
+                    <h3 style={{color: "white"}}>Placeholder</h3>
+                    <input className="item-form-add-image" type="text" name="image" placeholder="Image Url" onChange={this.onChange} />
+                </div>
+                <div className="item-form-row-one">
+                    <h2>Enter the details for the new item.</h2>
+                    <input className="item-form-name item-form-input" type="text" name="name" onChange={this.onChange} />
+                    <input className="item-form-category item-form-input" type="text" name="category" placeholder="Item Category" onChange={this.onChange} />
+                    <textarea className="item-form-detail item-form-input" name="description" placeholder="Item Description" onChange={this.onChange} />
+                </div>
+                <div className="item-form-row-two">
+                    <div className="item-form-row-two-unit-price">
+                        <input className="item-form-unit" type="number" name="unit" placeholder="Unit Size" onChange={this.onChange} />
+                        <input className="item-form-unit" type="number" name="price" placeholder="Unit Price" onChange={this.onChange} />
+                    </div>
+                    <div className="item-form-row-two-inventory">
+                    <input className="item-form-inventory" type="number" name="inventory" placeholder="Inventory Ct." onChange={this.onChange} />
+                    </div>
+                </div>
+                <div className="item-form-buttons">
+                    <Link to="/catalog"><Button type="delete" label="Cancel" /></Link>
+                    <Button as="submit" type="edit" label="Create" />
+                </div>
+            </form>
+        )} else {return null}
     }
 }
 
