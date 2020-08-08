f1301a878c5e2f88472c8dbfd471db9b67a92d50
“Atessadai” <Atessa.dailami@gmail.com>
2020-08-08 16:08:03 -0400

    sign up/login form style

--- src/components/SignUpForm/SignUpForm.js
+++ src/components/SignUpForm/SignUpForm.js
@@ -6,31 +6,31 @@ import './SignUpForm.css'
 class SignUpForm extends Component {
 
 
-  
 
-  render () {
-    if (this.props.isLoggedIn !== true){
-      
-    return (
-      <div>
-        <h2>Sign Up</h2>
 
-        <form>
-          <div>
-            <label htmlFor='email'>Email</label>
-            <input type='text' name='email' onChange={this.props.handleInput} />
-          </div>
+  render() {
+    if (this.props.isLoggedIn !== true) {
 
-          <div>
-            <label htmlFor='password'>Password</label>
-            <input type='text' name='password' onChange={this.props.handleInput} />
-          </div>
-          <input value='Submit' type='submit' onClick={this.props.handleSignUp} />
-        </form>
-      </div>
-    )
+      return (
+        <div className="login-form">
+
+          <form>
+            <h2 className="text-center">Sign Up</h2>
+            <div className="form-group">
+              <input type='text' name='email' placeholder='Email' className="inputfield" onChange={this.props.handleInput} />
+            </div>
+
+            <div className="form-group">
+              <input type='text' name='password' placeholder='Password' className="inputfield" onChange={this.props.handleInput} />
+            </div>
+            <div className="form-group">
+              <input value='Submit' type='submit' className="login-signup-btn" onClick={this.props.handleSignUp} />
+            </div>
+          </form>
+        </div>
+      )
     }
-    else {return <div><h1>Success! You're now signed up and logged in</h1></div>}
+    else { return <div><h1>Success! You're now signed up and logged in</h1></div> }
 
   }
 }
